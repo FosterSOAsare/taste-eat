836a7b61ff7844183bd0e30ee8a745d4c3c42e1d
fostersoasare <fostersoasare@gmail.com>
2023-03-15 12:35:17 +0000

    Create New Blog Page

---
+++ client/src/components/ImageInput/ImageInput.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { Box, Typography } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import { useTheme } from "@mui/material/styles";
+
+const ImageInput = ({ name, label, handleChange, sx }) => {
+	const [imageData, setImageData] = useState(null);
+	const theme = useTheme();
+
+	function readImage(image) {
+		const reader = new FileReader();
+		reader.readAsDataURL(image);
+		reader.onloadend = () => {
+			setImageData(reader.result);
+		};
+	}
+	return (
+		<Box className="addImage" sx={sx}>
+			<label htmlFor={name} className="flex items-center justify-start gap-[10px]">
+				<Box sx={{ width: "120px", height: "70px", backgroundColor: theme.palette.desc.main, display: "flex", alignItems: "center", justifyContent: "center" }}>
+					{!imageData && <AddIcon sx={{ fontSize: "28px" }} color="secondary" />}
+					{imageData && <img src={imageData} alt="Uploaded lead" className="w-full h-full" />}
+				</Box>
+				{imageData ? "Change " : "Add "}
+				{label}
+			</label>
+			<input
+				type="file"
+				accept="image/png, image/gif, image/jpeg"
+				aria-label={label}
+				onChange={(e) => {
+					handleChange(name, e.target.files);
+
+					if (e.target.files[0]) {
+						readImage(e.target.files[0]);
+					}
+				}}
+				id={name}
+				className="hidden"
+			/>
+		</Box>
+	);
+};
+
+export default ImageInput;
--- client/src/pages/Blogs/NewBlog.jsx
+++ client/src/pages/Blogs/NewBlog.jsx
@@ -1,26 +1,27 @@
-import React, { useState } from "react";
-import { Box, Container, Typography, useTheme, TextField } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Box, Container, Typography, useTheme, TextField, Button } from "@mui/material";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
-// import EditorBalloon from "@ckeditor/ckeditor5-build-balloon";
 
 import styles from "../../app.styles";
 import Title from "../../components/Title/Title";
 import PageDesc from "../../components/Header/PageDesc";
+import ImageInput from "../../components/ImageInput/ImageInput";
 
 const NewBlogPage = () => {
 	const theme = useTheme();
 	const [blogData, setBlogData] = useState({});
-	console.log(blogData);
 
 	function storeBlog(e) {
 		e.preventDefault();
 	}
 	function handleChange(name, value) {
+		console.log(name, value);
 		setBlogData((prev) => {
 			return { ...prev, [name]: value };
 		});
 	}
+
 	return (
 		<>
 			<PageDesc content="Create Blog" />
@@ -30,9 +31,7 @@ const NewBlogPage = () => {
 					<Typography variant="h1" sx={{ ...styles.title, fontSize: "28px", marginBlock: "10px" }}>
 						Write A New Blog
 					</Typography>
-
 					<TextField placeholder="Blog title" multiline maxRows={4} sx={styles.new__blog__text__field} onChange={(event) => handleChange("title", event.target.value)} />
-					{/* For the content of the blog */}
 					<CKEditor
 						editor={ClassicEditor}
 						data=""
@@ -42,29 +41,22 @@ const NewBlogPage = () => {
 						}}
 						config={{
 							placeholder: "Enter detailed blog content here",
-							style: {
-								definitions: {
-									options: [
-										{ model: "paragraph", title: "Paragraph", class: "ck-heading_paragraph" },
-										{ model: "heading1", view: "h1", title: "Heading 1", class: "ck-heading_heading1" },
-										{ model: "heading2", view: "h2", title: "Heading 2", class: "ck-heading_heading2" },
-									],
-								},
-							},
 						}}
-						onReady={(editor) => {
-							// You can store the "editor" and use when it is needed.
-							editor.editing.view.change((writer) => {
-								writer.setStyle("min-height", "300px", editor.editing.view.document.getRoot());
-								writer.setStyle("font-size", "inherit", editor.editing.view.document.getRoot());
-							});
-						}}
-						onFocus={(event, editor) => {}}
-						className="ck-editor"
 					/>
 
 					<Box sx={styles.blog__summary__container}>
 						<TextField placeholder="Blog summary" multiline maxRows={4} sx={{ ...styles.new__blog__text__field }} onChange={(event) => handleChange("summary", event.target.value)} />
+					</Box>
+					<TextField placeholder="Tag" sx={styles.new__blog__text__field} onChange={(event) => handleChange("tag", event.target.value)} />
+
+					<ImageInput name="image" label="Lead Image" handleChange={handleChange} sx={{ marginTop: "20px" }} />
+					<Box sx={{ display: "flex", gap: "20px", marginTop: "30px" }}>
+						<Button variant="contained" sx={{ color: theme.palette.white.main, ...styles.button }} color="secondary">
+							Publish
+						</Button>
+						<Button variant="outlined" sx={{ ...styles.button }} color="secondary">
+							Save Draft
+						</Button>
 					</Box>
 				</Container>
 			</Box>
