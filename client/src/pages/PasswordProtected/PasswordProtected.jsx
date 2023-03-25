04ff4825b1c206fb06c9e45b719436c1ad6f2e98
fostersoasare <fostersoasare@gmail.com>
2023-03-25 21:57:22 +0000

    Password protected page uses snackbar and error handling

--- client/src/pages/PasswordProtected/PasswordProtected.jsx
+++ client/src/pages/PasswordProtected/PasswordProtected.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import { Box, Container, Typography, TextField, Button } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
@@ -6,19 +6,41 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import styles from "../../app.styles";
 import { useAdminContext } from "../../context/AdminContext";
 import { httpValidateAdmin } from "../../hooks/requests/request";
+import { statusFunc } from "../../components/Snackbar/status.service";
 
 import PageDesc from "../../components/Header/PageDesc";
+import Error from "../../components/Error/Error";
+import Snackbar from "../../components/Snackbar/Snackbar";
 
 const PasswordProtectedPage = () => {
 	const theme = useTheme();
-	const { isAdmin, setIsAdmin, setLoading } = useAdminContext();
+	const { setIsAdmin } = useAdminContext();
+	const [status, statusDispatchFunc] = useReducer(statusFunc, { error: null, success: null, waiting: null });
+
 	let [password, setPassword] = useState("");
 
 	async function logInAdmin() {
-		let res = await httpValidateAdmin("password", password);
-		if (res.error) return;
-		setIsAdmin(true);
-		localStorage.setItem("admin-session", res._id);
+		try {
+			statusDispatchFunc({ type: "setWaiting" });
+			// Validate password
+			if (!password) {
+				statusDispatchFunc({ type: "setError", payload: "Please enter your password " });
+				return;
+			}
+			let res = await httpValidateAdmin("password", password);
+			if (res.error) {
+				statusDispatchFunc({ type: "setError", payload: res.error });
+				return;
+			}
+			statusDispatchFunc({ type: "setSuccess", payload: "Admin user authenticated successfully..." });
+			setTimeout(() => {
+				statusDispatchFunc({ type: "clearStatus" });
+				setIsAdmin(true);
+				localStorage.setItem("admin-session", res._id);
+			}, 1000);
+		} catch (error) {
+			statusDispatchFunc({ type: "setError", payload: error.message });
+		}
 	}
 	return (
 		<>
@@ -34,10 +56,20 @@ const PasswordProtectedPage = () => {
 					<Typography variant="p" sx={{ ...styles.desc, width: "85%", textAlign: "center", fontSize: "14px" }}>
 						This page is password protected. If you are the website admin, or have access to this page, please type your password below.
 					</Typography>
-					<TextField id="" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter password here" sx={{ width: "60%", marginBlock: "20px" }} type="password" />
-					<Button variant="contained" color="secondary" sx={{ ...styles.button, width: "60%" }} onClick={logInAdmin}>
-						Submit
+					<TextField
+						id=""
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						placeholder="Enter password here"
+						sx={{ width: "60%", marginBlock: "20px" }}
+						type="password"
+						onFocus={() => statusDispatchFunc({ type: "clearStatus" })}
+					/>
+					<Button variant="contained" color="secondary" sx={{ ...styles.button, width: "60%" }} onClick={logInAdmin} disabled={status.waiting ? true : false}>
+						{status.waiting ? "Waiting..." : "Submit"}
 					</Button>
+					{status.error && <Error text={status.error} sx={{ textAlign: "center", marginTop: "30px" }} />}
+					{status.success && <Snackbar text={status.success} close={() => statusDispatchFunc({ type: "clearStatus" })} />}
 				</Container>
 			</Box>
 		</>
