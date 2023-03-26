8c2248d850790629fbee057184f250be457ab14a
fostersoasare <fostersoasare@gmail.com>
2023-03-26 22:18:29 +0000

    Newsletter route test

--- server/routes/Newsletter/Newsletter.controller.js
+++ server/routes/Newsletter/Newsletter.controller.js
@@ -3,11 +3,15 @@ const { insertSubscription, getSubscriptions } = require("../../models/Newslette
 async function controllerInsertSubscription(req, res) {
 	let { email } = req.body;
 	try {
+		if (!email) {
+			res.status(400).json({ error: "Please provide all needed data" });
+			return;
+		}
+
 		await insertSubscription(email);
 		// Send email  notification to user for subscription
 		res.status(201).json({ success: "Newsletter subscription successful" });
 	} catch (e) {
-		console.log(e.message);
 		res.status(404).json({ error: e.message });
 	}
 }
---
+++ server/routes/Newsletter/Newsletter.test.js
@@ -0,0 +1,50 @@
+const request = require("supertest");
+const app = require("../../app");
+const { startMongoose, mongoose } = require("../../mongoose");
+
+describe("Test the Newsletter Collection", () => {
+	beforeAll(async () => {
+		await startMongoose();
+	});
+	afterAll(async () => {
+		await mongoose.connection.close();
+	});
+	describe("GET subscriptions", () => {
+		// it("should return success", async () => {
+		// 	let response = await request(app).get("/newsletter").expect(200);
+		// 	console.log(response);
+		// });
+	});
+	describe("POST subscription", () => {
+		it("should return an error when no email is set", async () => {
+			let response = await request(app).post("/newsletter").expect(400);
+			response = JSON.parse(response.text);
+			expect(response).toStrictEqual({ error: "Please provide all needed data" });
+		});
+		it("should return an error when email is already registered", async () => {
+			let response = await request(app)
+				.post("/newsletter")
+				.send({
+					email: "asare4ster@gmail.com",
+				})
+				.expect(404);
+			response = JSON.parse(response.text);
+			expect(response).toStrictEqual({ error: "Email has already been added to the subscription list" });
+		});
+		it("should return success", async () => {
+			let alp = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+			let ext = "";
+			for (let i = 0; i < 6; i++) {
+				let rand = Math.floor(Math.random() * alp.length);
+				ext += rand;
+			}
+
+			let response = await request(app)
+				.post("/newsletter")
+				.send({
+					email: `asare4ster${ext}@gmail.com`,
+				})
+				.expect(201);
+		});
+	});
+});
