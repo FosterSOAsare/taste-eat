602bb5dad27c9d45f0b175df7934c13c8b551635
fostersoasare <fostersoasare@gmail.com>
2023-03-30 09:40:38 +0000

    Blog preview refactor

--- client/src/pages/Blogs/BlogPreview.jsx
+++ client/src/pages/Blogs/BlogPreview.jsx
@@ -25,7 +25,7 @@ const BlogPreview = ({ title, imageUrl, summary, tag, content, date, image }) =>
 				<Title text={tag} />
 				<Title text={date} />
 			</Box>
-			<Typography variant="h1" sx={{ ...styles.title, fontSize: { xxs: "32px", md: "42px" }, width: { xxs: "100%", sm: "50%" }, textAlign: "center", marginBottom: "10px" }}>
+			<Typography variant="h1" sx={{ ...styles.title, fontSize: { xxs: "32px", md: "42px" }, width: { xxs: "100%", sm: "80%", md: "60%" }, textAlign: "center", marginBlock: "10px" }}>
 				{title}
 			</Typography>
 			<Typography variant="p" sx={{ ...styles.desc, width: { sm: "60%", xxs: "100%" }, textAlign: { md: "center" } }}>
