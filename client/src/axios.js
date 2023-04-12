28f0e1bdad234ba196d1760e420c847e6d735651
fostersoasare <fostersoasare@gmail.com>
2023-04-12 09:24:21 +0000

    Setting up axios interceptors to intercept each request and check for the jwt

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,9 +2,16 @@ import axios from "axios";
 const axiosInstance = axios.create({
 	baseURL: process.env.REACT_APP_API_BASE_URL,
 });
-let token = localStorage.getItem("admin-token");
-if (token) {
-	axiosInstance.defaults.headers.common["Authorization"] = "Bearer " + token;
-}
 
+// Always check that there is a token or not before sending the request
+// Also modifies the headers if necessary
+axiosInstance.interceptors.request.use((config) => {
+	const token = localStorage.getItem("admin-token");
+	if (token) {
+		config.headers["Authorization"] = `Bearer ${token}`;
+	} else {
+		delete config.headers["Authorization"];
+	}
+	return config;
+});
 export default axiosInstance;
